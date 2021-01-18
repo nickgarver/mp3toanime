d98d106c55509710feed14f06c5bf05984ab4054
nickgarver <nickgarver5@gmail.com>
2021-01-18 16:04:09 -0500

    mobile download bug fix maybe

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -125,7 +125,7 @@ const FileUpload = () => {
       method: 'GET',
       responseType: 'blob', // important
     }).then((response) => {
-       const url = window.URL.createObjectURL(new Blob([response.data]));
+       const url = window.URL.createObjectURL(new Blob([response.data], {type: 'video/mp4'}));
        const link = document.createElement('a');
        link.href = url;
        link.setAttribute('download', `${title}.mp4`); //or any other extension
